2d76dc256f7de36b84d8ffa547fac300cf149534
jc <emclab2011@gmail.com>
2014-02-03 13:22:51 -0600

    JC: added wf related fields to model and did migration. spec passes.

--- app/models/mfg_orderx/order.rb
+++ app/models/mfg_orderx/order.rb
@@ -1,15 +1,15 @@
 module MfgOrderx
   class Order < ActiveRecord::Base
     attr_accessor :product_info, :customer_name, :expedite_noupdate, :cancelled_noupdate, :completed_noupdate, :sample_required_noupdate, :quote_id_noupdate, 
-                  :quote_unit_price, :order_total, :order_wt, :unit_price, :product_wt
+                  :quote_unit_price, :order_total, :order_wt, :unit_price, :product_wt, :id_noupdate, :wf_comment
     attr_accessible :cancelled, :completed, :customer_po, :delivery_date, :expedite, :last_updated_by_id, :order_date, :order_requirement, :qty, 
-                    :quote_id, :sample_required, :shipping_date, :contract_required, :executioner_id, :rfq_id, :wfid,
+                    :quote_id, :sample_required, :shipping_date, :contract_required, :executioner_id, :rfq_id, :wfid, :wf_state,
                     :product_info, :customer_name, :quote_id_noupdate, :quote_unit_price, :order_total, :order_wt, :unit_price, :product_wt,
                     :as => :role_new
     attr_accessible :cancelled, :completed, :customer_po, :delivery_date, :expedite, :last_updated_by_id, :order_date, :order_requirement, :qty, 
-                    :sample_required, :shipping_date, :contract_required, :executioner_id, :wfid,
+                    :sample_required, :shipping_date, :contract_required, :executioner_id, :wfid, :wf_state,
                     :product_info, :customer_name, :expedite_noupdate, :cancelled_noupdate, :completed_noupdate, :sample_required_noupdate, :quote_id_noupdate, 
-                    :quote_unit_price, :order_total, :order_wt, :unit_price, :product_wt,
+                    :quote_unit_price, :order_total, :order_wt, :unit_price, :product_wt, :id_noupdate, :wf_comment,
                     :as => :role_update
     belongs_to :last_updated_by, :class_name => 'Authentify::User'
     belongs_to :executioner, :class_name => 'Authentify::User' 
