e02ffd246ddc68c5944e793cd3e3e65295ffb3a7
jc <emclab2011@gmail.com>
2014-03-11 01:03:44 -0500

    JC: added mfg batch list to order index view and modified init file accordingly. More CN. spec passes.

--- lib/mfg_orderx.rb
+++ lib/mfg_orderx.rb
@@ -1,7 +1,7 @@
 require "mfg_orderx/engine"
 
 module MfgOrderx
-  mattr_accessor :quote_class, :rfq_class, :customer_class, :rfq_engine, :sample_index_path, :show_rfq_path
+  mattr_accessor :quote_class, :rfq_class, :customer_class, :rfq_engine, :sample_index_path, :show_rfq_path, :mfg_batch_index_path
   
   def self.quote_class
     @@quote_class.constantize
