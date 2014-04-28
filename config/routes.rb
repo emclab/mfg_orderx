673c11ae8c78994b4d8378d9545aaaa5ee041103
jc <emclab2011@gmail.com>
2014-04-28 16:51:22 -0500

    JC: added search action/stats in routes.rb. spec passes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 MfgOrderx::Engine.routes.draw do
-  resources :orders
+  resources :orders do
+    collection do
+      get :search
+      put :search_results
+      get :stats
+      put :stats_results
+    end
+  end
 
   root :to => 'orders#index'
 end
